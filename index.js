6b610e4af4b248285fa7e62e5d5d7c4ac9ff58e8
Mateo Fernandez <56394308+MateoF01@users.noreply.github.com>
2023-07-25 11:07:56 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const ticketLoginUpdater = require('./lib/ticketLoginUpdater.js');
+const createLoginTicket = require("./lib/createLoginTicket.js");
+const getToken = require("./lib/getToken.js")
+
+const  miLoginTicketRequest = 'files/MiLoginTicketRequest.xml';
+
+//ticketLoginUpdater.updateGenerationAndExpirationTime(miLoginTicketRequest);
+
+//createLoginTicket();
+
+getToken();
+
+
